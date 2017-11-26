32c3d00820a78321070c750850c3ca99ce1d3ea6
Walker <walkerj.dunn@gmail.com>
2017-11-26 21:37:37 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -13,3 +13,8 @@ function destructivelyAddElementToBeginningOfArray(array,element) {
   array.unshift(element)
   return array
 }
+
+function addElementToEndOfArray(array,element) {
+  array=[array...,element]
+  return array
+}
