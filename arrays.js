e557e3ecdc3b02728b5d1513b618f53d40251f8f
Walker <walkerj.dunn@gmail.com>
2017-11-26 21:31:02 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -8,6 +8,7 @@ function addElementToBeginningOfArray(array,element) {
 array=[element,...array]
 return array
 }
+
 function destructivelyAddElementToBeginningOfArray(array,element) {
   array.unshift(element)
 })
