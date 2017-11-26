f2d9864cb0cda075678d3b20c3dcaabcfa600cd6
Walker <walkerj.dunn@gmail.com>
2017-11-26 21:55:10 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -30,7 +30,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
   return array
 }
 function removeElementFromBeginningOfArray(array){
-  array.slice(1)
+  array = array.slice(1)
   return array
 }
 function destructivelyRemoveElementFromEndOfArray(array){
