2cf31c57597e8e87dc4d99292d278f83b2931a09
Walker <walkerj.dunn@gmail.com>
2017-11-26 20:59:56 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,11 @@
+var chocolateBars = [
+  "snickers",
+"hundred grand",
+"kitkat",
+"skittles"
+]
+var thingtoadd = "3 musketeers"
+function addElementToBeginningOfArray(){
+  [chocolateBars..., ${thingtoadd}]
+}
+function destructivelyAddElementToBeginningOfArray([chocolatebars...,])
